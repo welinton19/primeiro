54fee327287374de92ce0f50a61aa87b7637f2b8
welinton19 <133980158+welinton19@users.noreply.github.com>
2023-07-12 11:53:27 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var setadireita=window.document.getElementById("setadireita")
+var leonardo = window.document.getElementById("leonardo");
+var samantha = window.document.getElementById("samantha");
+var bruna = window.document.getElementById("bruna");
+var setaesquerda =window.document.getElementById("setaesquerda")
+
+function RolarParaDireita() {
+    leonardo.style = "display:none"
+    bruna.style = "display:flex"
+    setaesquerda.style= "display:flex; margin-top:55px"
+    setadireita.style= "display:none"
+
+}
+
+
+function RolarParaEsquerda() {
+    leonardo.style = "display:flex"
+    bruna.style = "display:none"
+    setaesquerda.style= "display:none; margin-top:55px"
+    setadireita.style= "display:flex"
+
+}
+ 
